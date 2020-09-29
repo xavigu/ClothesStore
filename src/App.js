e66c9c0559b16dc5f8618c24def1ae8305b1f8c2
xavigu <xavigu91@gmail.com>
2020-09-29 19:43:39 +0200

    Enable app to know when there is authentication changes

--- src/App.js
+++ src/App.js
@@ -8,18 +8,46 @@ import SignPage from './pages/sign/sign.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  // method to unsubscribe
+  unsubcribeFromAuth = null;
+
+  // onAuthStateChanged gives back a function that when we call close the subscription
+  componentDidMount(){
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    });
+  };
+
+  componentWillUnmount(){
+    this.unsubcribeFromAuth();
+  }
+
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
