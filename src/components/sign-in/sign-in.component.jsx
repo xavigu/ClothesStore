ccbf262a9fba8e53f783600dbb2245ab750aca9d
xavigu <xavigu91@gmail.com>
2020-09-29 20:28:14 +0200

    Add logic to have different styles to google signin button, Add sign/signout buttons in the header and some styles signin buttons form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,12 +50,14 @@ export default class SignIn extends React.Component {
             value={this.state.password}
             handleChange= {this.handleChange} 
             required/>
-          <CustomButton type="submit">
-            Sign in
-          </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type="submit">
+              Sign in
+            </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>  
         </form>
       </div>
     )
