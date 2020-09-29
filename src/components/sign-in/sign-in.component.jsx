7bc3301b93c8269f4fb34ba394c401c2604afca9
xavigu <xavigu91@gmail.com>
2020-09-29 13:50:07 +0200

    Create FormInput component to use it in the sign-in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import FormInput from '../form-input/form-input.component';
+
+import  './sign-in.styles.scss';
 
 export default class SignIn extends React.Component {
   constructor(props){
@@ -29,20 +32,20 @@ export default class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input 
+          <FormInput 
             name="email" 
-            type="email" 
+            type="email"
+            label="email" 
             value={this.state.email} 
-            onChange= {this.handleChange}
+            handleChange= {this.handleChange}
             required/>
-          <label>Email</label>
-          <input 
+          <FormInput 
             name="password" 
             type="password" 
+            label="password" 
             value={this.state.password}
-            onChange= {this.handleChange} 
+            handleChange= {this.handleChange} 
             required/>
-          <label>Password</label>
 
           <input type="submit" value='Submit Form'/>
         </form>
