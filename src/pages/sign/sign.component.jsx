c3cf3ca5287aa740ae4dfaf4c51f3256fba1983b
xavigu <xavigu91@gmail.com>
2020-10-01 18:29:38 +0200

    Add signup component to the sign page and add some styles

--- src/pages/sign/sign.component.jsx
+++ src/pages/sign/sign.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 import "./sign.styles.scss";
 
 const SignPage = ({}) => (
   <div className='sign'>
     <SignIn />
+    <SignUp />
   </div>
 )
 
