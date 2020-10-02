c66d328bb73ee5be14753589936e49973c2963db
xavigu <xavigu91@gmail.com>
2020-10-02 13:59:27 +0200

    Create redux store and use it like a property in Provider component of index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+import store from './redux/store';
 
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter> 
