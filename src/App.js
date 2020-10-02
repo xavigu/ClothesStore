068a916138502344512167fb70c1110d326a4d1f
xavigu <xavigu91@gmail.com>
2020-10-02 17:50:20 +0200

    Instead of setSate, dispatch currentUser with the action function whenever you want update user

--- src/App.js
+++ src/App.js
@@ -9,39 +9,31 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   // method to unsubscribe
   unsubcribeFromAuth = null;
 
   // onAuthStateChanged gives back a function that when we call close the subscription
   componentDidMount(){
+    const {setCurrentUser} = this.props;
+
     this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         // check data in database of that userRef
         userRef.onSnapshot(snapshot => {
-          // pass de log as second parameter because is async and that function only calls after setState
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapshot.id,
               ...snapshot.data()
-            }
-          }, () => {
-            console.log(this.state);
-          })
+          });
         });
       }
       // setState to null if there is not userAuth
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   };
 
@@ -62,6 +54,10 @@ class App extends React.Component {
       </div>
     );
   }
-}
+};
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
