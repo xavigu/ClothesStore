60bbd2345c4f01f7455ae5bdd70414dad78b2f8f
xavigu <xavigu91@gmail.com>
2020-10-03 17:26:54 +0200

    Redirect to home page when you are trying to go to the sign page and you are logged. Create new file when we store the user types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 
@@ -49,15 +49,32 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignPage} />
+          <Route 
+            exact 
+            path='/signin' 
+            render={() => 
+             this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignPage/>
+              ) 
+            } 
+          />
         </Switch>
       </div>
     );
   }
 };
 
+const mapStateToProps = ({user}) => ({
+  currentUser : user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(App);
