ca9a5b5a08bdf0d83c77fa142c39057b2f4fda76
xavigu <xavigu91@gmail.com>
2020-10-04 21:03:00 +0200

    Add cart redux files, import into root-reducer, add cart action when click in cart-icon and when the hidden field of state is false show the cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import "./header.styles.scss";
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />
@@ -30,13 +30,19 @@ const Header = ({currentUser}) => (
       }
       <CartIcon/>
     </div>
-    <CartDropdown/>
+    {
+      hidden ? 
+      null 
+      :
+      <CartDropdown/>
+    }
   </div>
 );
 
 // we get the root reducer state
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,9 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import userReducer from './user/user.reducer';
+import cartReducer from "./cart/cart.reducer";
+import userReducer from "./user/user.reducer";
 
-
-export default combineReducers ({
-  user: userReducer
-})
+export default combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
