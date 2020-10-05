6344d2b98231e0178c27e5848b80d875252c0604
xavigu <xavigu91@gmail.com>
2020-10-05 17:42:31 +0200

    Add new logic when we add the same item increase the value of a new key (quantity) of the item object

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  // if dont exist the Item in cartItems array added with a quantity property with value 1
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
