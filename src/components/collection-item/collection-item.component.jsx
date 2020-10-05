42e260fd5863e96e31768b27a15eff03ecc791c4
xavigu <xavigu91@gmail.com>
2020-10-05 13:31:38 +0200

    Add new class to custom-button when have property inverted and some styles to collection items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component';
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
@@ -11,7 +13,8 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
       <div className='collection-footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}$</span>
-      </div>  
+      </div>
+      <CustomButton inverted>Add to cart</CustomButton>
   </div>
 )
 
