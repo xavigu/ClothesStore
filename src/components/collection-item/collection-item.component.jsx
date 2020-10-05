803edf33f93c01ac403f298ace02dcfc69645967
xavigu <xavigu91@gmail.com>
2020-10-05 14:00:59 +0200

    Create ADD_ITEM action and dispatch when you click the button of the collection-item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+  const {name, price, imageUrl} = item;
+  return ( 
   <div className='collection-item'>
     <div 
       className='image'
@@ -14,8 +18,14 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         <span className='name'>{name}</span>
         <span className='price'>{price}$</span>
       </div>
-      <CustomButton inverted>Add to cart</CustomButton>
-  </div>
-)
+      <CustomButton inverted onClick= {() => addItem(item)} >Add to cart</CustomButton>
+  </div> 
+)};
 
-export default CollectionItem;
+// The prop addItem is a function that assign the dispatch action addItem
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+// null because we are not taking any map state to props
+export default connect(null, mapDispatchToProps)(CollectionItem);
