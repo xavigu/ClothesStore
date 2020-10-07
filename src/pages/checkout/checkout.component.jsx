67438f12f11705bb3c015a9493a15dc4d7c16f0d
xavigu <xavigu91@gmail.com>
2020-10-07 17:53:35 +0200

    Add persist-redux library and create a persistReducer to persist the data of the cart in our App

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -44,7 +44,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       )
     }
     <div className="total">
-        <span>Total: ${total}</span>
+        <span>Total: {total}$</span>
     </div>
   </div>
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import cartReducer from "./cart/cart.reducer";
-import userReducer from "./user/user.reducer";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default combineReducers({
+import cartReducer from './cart/cart.reducer';
+import userReducer from './user/user.reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
