9dcb086580533f0a69e3c96ed88c7f4160576343
xavigu <xavigu91@gmail.com>
2020-10-08 19:10:19 +0200

    Add collection Items into the collection page and their styles

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match, collection }) => (
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
   <div className="collection-page">
-    <h2>{ match.params.collectionId }</h2>
-        <CollectionPreview key={collection.id} {...collection} />
+    <h2 className='title'>{title}</h2>
+    <div className="items">
+      {
+        items.map(item => (
+            <CollectionItem key={item.id} item={item} />
+        ))
+      }
+    </div>
   </div>
-);
+ );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   // This selector needs a part of the state depending on the URL parameter
