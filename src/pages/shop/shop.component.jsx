a98af56687ecec200d0818015d756ed393de3eed
xavigu <xavigu91@gmail.com>
2020-10-08 13:43:21 +0200

    Create collections-overview component to abstract and use in the shop component and can reuse it in another categories pages later

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { selectShopCollections } from "../../redux/shop/shop.selectors.js";
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = ({ collections }) =>  (
+const ShopPage = () =>  (
   <div className='shop-page'>
-    {
-      collections.map(({id, ...otherProps}) => (
-        <CollectionPreview key={id} {...otherProps} />
-      ))
-    }
+    <CollectionsOverview />
   </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
