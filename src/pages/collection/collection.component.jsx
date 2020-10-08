ce682aafb753f6544b7ea412be43ca4e0688d057
xavigu <xavigu91@gmail.com>
2020-10-08 18:19:34 +0200

    Create a selector to get the collection depending the params of the URL and show the correct collection in the collection component according to the URL params

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ match, collection }) => (
   <div className="collection-page">
     <h2>{ match.params.collectionId }</h2>
+        <CollectionPreview key={collection.id} {...collection} />
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  // This selector needs a part of the state depending on the URL parameter
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
