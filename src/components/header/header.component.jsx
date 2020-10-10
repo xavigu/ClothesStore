0aa1f98a54bf7f3fbb080d079517bb9c748b9f2a
xavigu <xavigu91@gmail.com>
2020-10-10 20:17:15 +0200

    Add styled-components and use in header and homepage to css the elements that render it

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,35 +12,37 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import "./header.styles.scss";
+import { HeaderContainer, LogoContainer, OptionDiv, OptionLink, OptionsContainer } from './header.styles';
 
 const Header = ({currentUser, hidden}) => (
-  <div className='header'>
-    <Link className='logo-container' to="/">
+  <HeaderContainer>
+    <LogoContainer to="/">
       <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to="/shop">
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="/shop">
         SHOP
-      </Link>
-      <Link className='option' to="/contact">
+      </OptionLink>
+      <OptionLink to="/contact">
         CONTACT
-      </Link>
+      </OptionLink>
       {
         currentUser ?
-        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
         :
-        <Link className='option' to='/signin'>SIGN IN</Link>
+        <OptionLink to='/signin'>SIGN IN</OptionLink>
       }
       <CartIcon/>
-    </div>
+    </OptionsContainer>
     {
       hidden ? 
       null 
       :
       <CartDropdown/>
     }
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
+import { HomePageContainer } from "./homepage.styles";
 
 const HomePage = () => (
   <div className="homepage">
-    <Directory />
+    <HomePageContainer>
+      <Directory />
+    </HomePageContainer> 
   </div>
 );
 
