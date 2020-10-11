f067b2dfb72b06b56d6c2826de2e8efe12d1f673
xavigu <xavigu91@gmail.com>
2020-10-11 18:37:23 +0200

    Create a function to add collections into firestore instead of do it manually

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,25 +4,25 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyA2nXiFIYyHPDRW_W9jtG7adzaLiZ-hPUg",
-  authDomain: "clothes-store-a35af.firebaseapp.com",
-  databaseURL: "https://clothes-store-a35af.firebaseio.com",
-  projectId: "clothes-store-a35af",
-  storageBucket: "clothes-store-a35af.appspot.com",
-  messagingSenderId: "68458598170",
-  appId: "1:68458598170:web:d1e467b033582889bea6a3",
-  measurementId: "G-QB4SVP0WBX"
+  apiKey: 'AIzaSyA2nXiFIYyHPDRW_W9jtG7adzaLiZ-hPUg',
+  authDomain: 'clothes-store-a35af.firebaseapp.com',
+  databaseURL: 'https://clothes-store-a35af.firebaseio.com',
+  projectId: 'clothes-store-a35af',
+  storageBucket: 'clothes-store-a35af.appspot.com',
+  messagingSenderId: '68458598170',
+  appId: '1:68458598170:web:d1e467b033582889bea6a3',
+  measurementId: 'G-QB4SVP0WBX',
 };
 
-export const createUserProfileDocument = async(userAuth, additionalData) => {
-  if(!userAuth) return;
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapshot = await userRef.get();
 
   // check if there is any data of that userRef in the database(snapshot), if not create de new user in database
-  if(!snapshot.exists){
+  if (!snapshot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
@@ -31,21 +31,36 @@ export const createUserProfileDocument = async(userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
-      })
+        ...additionalData,
+      });
     } catch (error) {
       console.log('error creating user', error.message);
     }
   }
 
   return userRef;
-}
+};
+
+// Function to add a collection in Firebase passin a string collectionKey, and the array of objects to add
+export const addCollectionsAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log('collection Ref:', collectionRef);
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
 
 // get access to google auth in the auth library
 const provider = new firebase.auth.GoogleAuthProvider();
