59150de7ba297497ea927abe8bc5e20ab25b3ce1
xavigu <xavigu91@gmail.com>
2020-10-13 13:55:06 +0200

    Create new selector to use in shopPage and only render CollectionPage when shop.collections state is not null

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,7 +4,7 @@ import { Route } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
 
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
-import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
 
 import CollectionPage from '../collection/collection.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
@@ -22,21 +22,22 @@ class ShopPage extends React.Component{
   }
 
   render(){
-   const { match, isCollectionFetching } = this.props;
+   const { match, isFetchingCollections, isCollectionsLoaded } = this.props;
 
    return(
     <div className='shop-page'>
       <Route exact path={`${match.path}`} 
-             render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props}/>}/>
+             render={(props) => <CollectionsOverviewWithSpinner isLoading={isFetchingCollections} {...props}/>}/>
       <Route path={`${match.path}/:collectionId`} 
-             render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props}/>}/>
+             render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props}/>}/>
     </div>
    )
   }
 }
 
 const mapStateToProps =  createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetching
+  isFetchingCollections: selectIsCollectionFetching,
+  isCollectionsLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
