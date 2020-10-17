8b10458f5b214ef3a1bd2d060de025af4cd12982
xavigu <xavigu91@gmail.com>
2020-10-17 13:45:29 +0200

    Update App and shop page from class to functional component and use useEffect hook instead of componentDidMount function

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Route } from "react-router-dom";
 
@@ -8,26 +8,21 @@ import CollectionsOverviewContainer from "../../components/collections-overview/
 import CollectionPageContainer from "../collection/collection.container";
 
 
-class ShopPage extends React.Component{
-
-  componentDidMount() {
-    // we get the function from the props and run it
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  // only fires if the result of fetchCollectionsStart changes
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart])
 
-  render(){
-   const { match } = this.props;
+  return(
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} 
+            component={CollectionsOverviewContainer}/>
+    <Route path={`${match.path}/:collectionId`} 
+            component={CollectionPageContainer}/>
+  </div>
+  )
 
-   return(
-    <div className='shop-page'>
-      <Route exact path={`${match.path}`} 
-             component={CollectionsOverviewContainer}/>
-      <Route path={`${match.path}/:collectionId`} 
-             component={CollectionPageContainer}/>
-    </div>
-   )
-  }
 }
 
 const mapDispatchToProps = dispatch => ({
