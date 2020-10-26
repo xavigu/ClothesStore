be8e41c8d4f19cfe87970498d410efeb1305c909
xavigu <xavigu91@gmail.com>
2020-10-26 12:57:26 +0100

    Add lazy loading to the components and spinner component while is loading

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { connect } from "react-redux";
 import { Route } from "react-router-dom";
+import Spinner from "../../components/spinner/spinner.component";
 
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
-import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
-import CollectionPageContainer from "../collection/collection.container";
-
+const CollectionsOverviewContainer = lazy(() => import("../../components/collections-overview/collections-overview.container"));
+const CollectionPageContainer = lazy(() => import("../collection/collection.container"));
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   // only fires if the result of fetchCollectionsStart changes
@@ -16,10 +16,12 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
   return(
   <div className='shop-page'>
-    <Route exact path={`${match.path}`} 
-            component={CollectionsOverviewContainer}/>
-    <Route path={`${match.path}/:collectionId`} 
-            component={CollectionPageContainer}/>
+    <Suspense fallback={<Spinner/>}>
+      <Route exact path={`${match.path}`} 
+              component={CollectionsOverviewContainer}/>
+      <Route path={`${match.path}/:collectionId`} 
+              component={CollectionPageContainer}/>
+    </Suspense>
   </div>
   )
 
