5832b4a7ee88074939b1da1dede20551d89b8852
xavigu <xavigu91@gmail.com>
2021-06-08 17:58:43 +0200

    Add svg in contact page with animations

--- client/src/pages/contact/contact.component.jsx
+++ client/src/pages/contact/contact.component.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import literals from "./../../literals";
 
 import ContactLogo from "../../components/contact-logo/contact-logo.component";
+import {ReactComponent as TrianglesIcon} from './../../assets/icons/triangles.svg';
 import "./contact.styles.scss"
 
 const contactItems = [
@@ -10,8 +11,26 @@ const contactItems = [
   'gmail'
 ];
 
+const changeColorTriangles = () =>
+{
+  const svg = document.getElementById('triangles');
+  svg.onclick = (e) => {
+    const colors = ['red', 'blue', 'green', 'orange', 'purple'];
+    const rand = () => colors[Math.floor(Math.random() * colors.length)];
+    console.log(rand);
+    document.documentElement.style.cssText = `
+      --dark-color: ${rand()};
+      --light-color: ${rand()};
+    `
+  }
+}
+
 const ContactPage = () => 
 {
+  useEffect(() => {
+    changeColorTriangles();
+  });
+
   return (
     <div className="contact-page">
       <h2 className='title'>{literals.CONTACT_TITLE}</h2>
@@ -20,12 +39,19 @@ const ContactPage = () =>
           {literals.CONTACT_DESCRIPTION}
         </div>
         <div className="contact-items">
-          <p>{literals.CONTACT_ITEMS_SUBTITLE}</p> 
-          {
-            contactItems.map((item) => (
-              <ContactLogo logo={item} />
-            ))
-          }
+          <span className="contact-subtitle" style={{marginRight:"8px"}}>
+              {literals.CONTACT_ITEMS_SUBTITLE}
+          </span>
+          <span className="contact-triangles">
+              <TrianglesIcon/>
+          </span>
+          <div className="contact-logos">
+            {
+              contactItems.map((item) => (
+                <ContactLogo logo={item} />
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
