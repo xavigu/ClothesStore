f11bd798b2d8fba1e8aa00a85b59790d5cbaa05c
xavigu <xavigu91@gmail.com>
2022-10-10 12:38:42 +0200

    Update one literal

--- client/src/literals.js
+++ client/src/literals.js
@@ -1,6 +1,6 @@
 const literals = {
   CONTACT_TITLE: 'Contact',
-  CONTACT_DESCRIPTION: 'Welcome to my personal clothing store website in React with Redux, Hooks, GraphQL, ContextAPI, Stripe, Firebase...  ',
+  CONTACT_DESCRIPTION: 'Welcome to my personal clothing store website in React with Redux, Hooks, GraphQL, ContextAPI, Stripe, Firebase and much more!',
   CONTACT_ITEMS_SUBTITLE: 'How to contact me: '
 }
 
